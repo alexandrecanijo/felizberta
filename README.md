3ee49a5b165f88c0325bdb77de6a2cec1f66e21e
Alexandre Canijo <alex@alexandrecanijo.com>
2019-09-11 00:55:15 +0100

    Updating Readme and index page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Felizberta
-Felizberta
+[![Netlify Status](https://api.netlify.com/api/v1/badges/961e533a-9808-438f-ad76-97f8ea9cbfe6/deploy-status)](https://app.netlify.com/sites/felizberta/deploys)  
+> A Nuxt.js static website
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo />
       <h1 class="title">
-        felizbertanuxt
+        Felizberta
       </h1>
       <h2 class="subtitle">
-        My excellent Nuxt.js project
+        A future project, based on Nuxt.js...
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
